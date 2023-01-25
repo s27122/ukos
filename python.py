e97eecc04d14f399be62ecf1056b9b38bf98152b
Artem Stakhovski <s27297@pjwstk.edu.pl>
2023-01-25 16:19:33 +0100

    dodalem srodek

--- python.py
+++ python.py
@@ -5,3 +5,6 @@ def read_file(file_path):
 	with open(file_path, "r") as file:
 		return file.read()
 
+def save_to_file(file_path, data):
+        with open(file_path, "w") as file:
+                file.write(data)
